38deb158bc630048cdc72fe14154bb83aa6f0e34
Dario Varriale <dariovarriale@rocketmail.com>
2025-03-09 16:03:03 +0100

    Update checkCode provider to return GameSession object instead of String

--- lib/features/welcome_screen/data/check_session_code_provider.dart
+++ lib/features/welcome_screen/data/check_session_code_provider.dart
@@ -1,22 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:tombola/models/game_session.dart';
 import 'package:tombola/utils/extensions.dart';
 
 part 'check_session_code_provider.g.dart';
 
 @riverpod
-Future<String?> checkCode(Ref ref, String code) async {
+Future<GameSession?> checkCode(Ref ref, String code) async {
   try {
     final sessionsCollection = FirebaseFirestore.instance.sessions;
     final docSnapshot = await sessionsCollection.doc(code).get();
 
     if (docSnapshot.exists) {
-      return code;
+      final data = docSnapshot.data() as Map<String, dynamic>;
+      data['id'] = docSnapshot.id;
+      return GameSession.fromJson(data);
     }
+
+    return null;
   } catch (e) {
     return null;
   }
-
-  return null;
 }
