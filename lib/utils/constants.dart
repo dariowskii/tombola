9f89c20e1a52adea18d47fb3f7525474871ac144
Dario Varriale <dariovarriale@rocketmail.com>
2025-03-09 14:37:40 +0100

    Add AssetMedia enum for asset paths in constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,3 +3,12 @@ const kRaffleCardColumnNumber = 9;
 const kRaffleCardSize = 30;
 
 const kMaxExtractableNumbers = 90;
+
+// ASSETS
+
+enum AssetMedia {
+  welcomeBackground('assets/img/welcome_bg.jpg');
+
+  const AssetMedia(this.path);
+  final String path;
+}
