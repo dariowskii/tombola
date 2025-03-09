c821c3765fd85d0dd5036c4a2ceba8603e5c5b9f
Dario Varriale <dariovarriale@rocketmail.com>
2025-03-09 17:50:16 +0100

    Refactor LoginScreen to use StatefulWidget; implement email/password login with Firebase authentication

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,20 +1,185 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:tombola/utils/constants.dart';
+import 'package:tombola/utils/extensions.dart';
+import 'package:tombola/utils/logger.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late final _emailController = TextEditingController();
+  late final _passwordController = TextEditingController();
+
+  late final _formKey = GlobalKey<FormState>();
+
+  var _isLogging = false;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _tryLogin() async {
+    if (_formKey.currentState?.validate() == false) {
+      return;
+    }
+
+    setState(() {
+      _isLogging = true;
+    });
+
+    try {
+      final email = _emailController.text;
+      final password = _passwordController.text;
+
+      if (email.isEmpty || password.isEmpty) {
+        return;
+      }
+
+      final user = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      Logger.general.debug('User: $user');
+    } on FirebaseAuthException catch (e) {
+      final message = e.message ?? 'Errore sconosciuto';
+      if (mounted) {
+        context.showSnackBar(message);
+      }
+    } catch (e) {
+      if (mounted) {
+        context.showSnackBar(e.toString());
+      }
+    } finally {
+      if (mounted) {
+        setState(() {
+          _isLogging = false;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text('Admin Login'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate to the home screen
-          },
-          child: const Text('Login'),
+      body: Padding(
+        padding: EdgeInsets.all(
+          Spacing.medium.value,
+        ),
+        child: Center(
+          child: AutofillGroup(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    enabled: !_isLogging,
+                    controller: _emailController,
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.emailAddress,
+                    onTap: _isLogging
+                        ? null
+                        : () {
+                            Scrollable.ensureVisible(
+                              context,
+                              duration: 500.ms,
+                              curve: Curves.decelerate,
+                            );
+                          },
+                    onTapUpOutside: (_) {
+                      FocusManager.instance.primaryFocus?.unfocus();
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Inserisci la l\'email';
+                      }
+
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      label: Text('Email'),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(16),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Spacing.small.h,
+                  TextFormField(
+                    enabled: !_isLogging,
+                    controller: _passwordController,
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    onTap: _isLogging
+                        ? null
+                        : () {
+                            Scrollable.ensureVisible(
+                              context,
+                              duration: 500.ms,
+                              curve: Curves.decelerate,
+                            );
+                          },
+                    onTapUpOutside: (_) {
+                      FocusManager.instance.primaryFocus?.unfocus();
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Inserisci la password';
+                      }
+
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      label: Text('Password'),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(16),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Spacing.large.h,
+                  SizedBox(
+                    width: double.infinity,
+                    child: FilledButton(
+                      onPressed: _isLogging ? null : _tryLogin,
+                      child: _isLogging
+                          ? Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text('Entra'),
+                                Spacing.medium.w,
+                                SizedBox(
+                                  width: 16,
+                                  height: 16,
+                                  child: CircularProgressIndicator(
+                                    strokeWidth: 2,
+                                    color: context.colorScheme.onPrimary,
+                                  ),
+                                ),
+                              ],
+                            )
+                          : const Text('Entra'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
