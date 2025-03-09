13b36d00a5c0cdd0c1fb269361fcd0351c2311ba
Dario Varriale <dariovarriale@rocketmail.com>
2025-03-09 14:27:00 +0100

    Add theme and text theme extensions to BuildContext

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -3,4 +3,8 @@ import 'package:flutter/material.dart';
 extension BuildContextExtension on BuildContext {
   double get width => MediaQuery.sizeOf(this).width;
   double get height => MediaQuery.sizeOf(this).height;
+
+  ThemeData get theme => Theme.of(this);
+  TextTheme get textTheme => theme.textTheme;
+  ColorScheme get colorScheme => theme.colorScheme;
 }
