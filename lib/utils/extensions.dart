0c11bbcd6a7d417b89e54da7e372603eb22b618f
Dario Varriale <dariovarriale@rocketmail.com>
2025-03-09 15:05:58 +0100

    Add Duration extensions for easier time conversions

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -8,3 +8,13 @@ extension BuildContextExtension on BuildContext {
   TextTheme get textTheme => theme.textTheme;
   ColorScheme get colorScheme => theme.colorScheme;
 }
+
+extension DurationExtensions on num {
+  Duration get microseconds => Duration(microseconds: round());
+  Duration get ms => (this * 1000).microseconds;
+  Duration get milliseconds => (this * 1000).microseconds;
+  Duration get seconds => (this * 1000 * 1000).microseconds;
+  Duration get minutes => (this * 1000 * 1000 * 60).microseconds;
+  Duration get hours => (this * 1000 * 1000 * 60 * 60).microseconds;
+  Duration get days => (this * 1000 * 1000 * 60 * 60 * 24).microseconds;
+}
