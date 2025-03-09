1599039474327c7bda1728c22db52cabd22c77b0
Dario Varriale <dariovarriale@rocketmail.com>
2025-03-09 18:04:27 +0100

    Refactor AdminScreen to use StatefulWidget; implement logout confirmation dialog and user logout functionality

--- lib/screens/admin_screen.dart
+++ lib/screens/admin_screen.dart
@@ -1,13 +1,72 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:tombola/router/routes.dart';
+import 'package:tombola/utils/extensions.dart';
 
-class AdminScreen extends StatelessWidget {
+class AdminScreen extends StatefulWidget {
   const AdminScreen({super.key});
+
+  @override
+  State<AdminScreen> createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  void _askLogoutConfirmation() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Esci'),
+          content: const Text('Sei sicuro di voler uscire?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'No',
+                style: TextStyle(
+                  color: context.textTheme.bodySmall?.color,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: _logoutUser,
+              child: Text(
+                'Si',
+                style: TextStyle(
+                  color: context.colorScheme.error,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _logoutUser() async {
+    Navigator.pop(context);
+    await FirebaseAuth.instance.signOut();
+    if (!mounted) return;
+
+    const WelcomeRoute().go(context);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Admin Screen'),
+        title: const Text('Admin'),
+        actions: [
+          IconButton(
+            onPressed: _askLogoutConfirmation,
+            icon: const Icon(
+              Icons.logout,
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: ElevatedButton(
