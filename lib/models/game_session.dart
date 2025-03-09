5ad2ae2677453b45b608826ba05351497b3519a7
Dario Varriale <dariovarriale@rocketmail.com>
2025-03-09 17:13:21 +0100

    Add extractedNumbers and raffleCards to GameSession; introduce RaffleCard model

--- lib/models/game_session.dart
+++ lib/models/game_session.dart
@@ -9,8 +9,22 @@ abstract class GameSession with _$GameSession {
     required String id,
     required bool isActive,
     required String eventName,
+    @Default([]) List<int> extractedNumbers,
+    @Default([]) List<RaffleCard> raffleCards,
   }) = _GameSession;
 
   factory GameSession.fromJson(Map<String, dynamic> json) =>
       _$GameSessionFromJson(json);
+}
+
+@freezed
+abstract class RaffleCard with _$RaffleCard {
+  const factory RaffleCard({
+    required String id,
+    required String recoverId,
+    required List<int> numbers,
+  }) = _RaffleCard;
+
+  factory RaffleCard.fromJson(Map<String, dynamic> json) =>
+      _$RaffleCardFromJson(json);
 }
