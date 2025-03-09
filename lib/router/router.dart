c7497de0cf2fcd39c7fd6357b31099388c6e319b
Dario Varriale <dariovarriale@rocketmail.com>
2025-03-09 17:52:29 +0100

    Set initial route based on Firebase authentication status in router

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -7,7 +8,14 @@ part 'router.g.dart';
 
 @riverpod
 GoRouter router(Ref ref) {
+  var initialLocation = const WelcomeRoute().location;
+
+  if (FirebaseAuth.instance.currentUser != null) {
+    initialLocation = const AdminRoute().location;
+  }
+
   return GoRouter(
     routes: $appRoutes,
+    initialLocation: initialLocation,
   );
 }
