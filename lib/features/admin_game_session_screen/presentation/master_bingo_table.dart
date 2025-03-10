8b21511c8f930ebd45beaab817c19eda514d96ac
Dario Varriale <dario.varriale@atobit.it>
2025-03-10 21:22:59 +0100

    Remove AngleType enum from master_bingo_table.dart and import it from utils

--- lib/features/admin_game_session_screen/presentation/master_bingo_table.dart
+++ lib/features/admin_game_session_screen/presentation/master_bingo_table.dart
@@ -1,38 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tombola/utils/angle_type_enum.dart';
 import 'package:tombola/utils/constants.dart';
 import 'package:tombola/utils/extensions.dart';
-
-enum AngleType {
-  topLeft,
-  topRight,
-  bottomLeft,
-  bottomRight,
-  none;
-
-  static AngleType fromNumber(int number) => switch (number) {
-        1 => AngleType.topLeft,
-        10 => AngleType.topRight,
-        81 => AngleType.bottomLeft,
-        90 => AngleType.bottomRight,
-        _ => AngleType.none,
-      };
-
-  BorderRadius get borderRadius => switch (this) {
-        AngleType.topLeft => const BorderRadius.only(
-            topLeft: Radius.circular(8),
-          ),
-        AngleType.topRight => const BorderRadius.only(
-            topRight: Radius.circular(8),
-          ),
-        AngleType.bottomLeft => const BorderRadius.only(
-            bottomLeft: Radius.circular(8),
-          ),
-        AngleType.bottomRight => const BorderRadius.only(
-            bottomRight: Radius.circular(8),
-          ),
-        AngleType.none => BorderRadius.zero,
-      };
-}
 
 class MasterBingoTable extends StatelessWidget {
   const MasterBingoTable({
