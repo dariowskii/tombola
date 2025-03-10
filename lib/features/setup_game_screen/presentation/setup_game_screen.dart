232876a6ead0b053f0364c36831e0579ad0a212f
Dario Varriale <dario.varriale@atobit.it>
2025-03-10 16:41:12 +0100

    Refactor SetupGameScreen to use ConsumerWidget and handle session state with Riverpod

--- lib/features/setup_game_screen/presentation/setup_game_screen.dart
+++ lib/features/setup_game_screen/presentation/setup_game_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tombola/features/setup_game_screen/presentation/error_state.dart';
+import 'package:tombola/providers/check_session_code_provider.dart';
+import 'package:tombola/utils/constants.dart';
 
-class SetupGameScreen extends StatelessWidget {
+class SetupGameScreen extends ConsumerWidget {
   const SetupGameScreen({
     super.key,
     required this.sessionId,
@@ -9,16 +13,26 @@ class SetupGameScreen extends StatelessWidget {
   final String sessionId;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncSession =
+        ref.watch(checkCodeProvider(sessionId)).unwrapPrevious();
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Setup Game'),
+        title: const Text('Imposta gioco'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: const Text('Start Game'),
-        ),
+      body: SafeArea(
+        child: switch (asyncSession) {
+          AsyncData(:final value) => Center(
+              child: Text('Sessione trovata: $value'),
+            ),
+          AsyncError(:final error) => SetupErrorState(
+              sessionId: sessionId,
+              error: error,
+            ),
+          _ => const Center(
+              child: CircularProgressIndicator(),
+            ),
+        },
       ),
     );
   }
