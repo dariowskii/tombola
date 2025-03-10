b8ef96455ea5dd4968e20a44376027b850161298
Dario Varriale <dario.varriale@atobit.it>
2025-03-10 16:56:06 +0100

    Update SetupGameBody to display game session details and active game badge

--- lib/features/setup_game_screen/data/setup_game_body.dart
+++ lib/features/setup_game_screen/data/setup_game_body.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tombola/models/game_session.dart';
+import 'package:tombola/utils/constants.dart';
+import 'package:tombola/utils/extensions.dart';
+import 'package:tombola/widgets/active_game_badge.dart';
 
 class SetupGameBody extends ConsumerWidget {
   const SetupGameBody({
     super.key,
-    required this.sessionId,
+    required this.gameSession,
   });
 
-  final String sessionId;
+  final GameSession gameSession;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(
+          Spacing.medium.value,
+        ),
+        child: Column(
+          children: [
+            const Text('Evento'),
+            Text(
+              gameSession.eventName,
+              style: context.textTheme.titleLarge?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Spacing.medium.h,
+            ActiveGameBadge(
+              isActive: gameSession.isActive,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
