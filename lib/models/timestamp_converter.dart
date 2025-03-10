668cd973c84ce60e93e469c8c942ac56ad0ed467
Dario Varriale <dario.varriale@atobit.it>
2025-03-10 22:12:28 +0100

    Make TimestampConverter nullable for better null safety handling

--- lib/models/timestamp_converter.dart
+++ lib/models/timestamp_converter.dart
@@ -1,12 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-class TimestampConverter implements JsonConverter<DateTime, Timestamp> {
+class TimestampConverter implements JsonConverter<DateTime?, Timestamp?> {
   const TimestampConverter();
 
   @override
-  DateTime fromJson(Timestamp value) => value.toDate().toLocal();
+  DateTime? fromJson(Timestamp? value) => value?.toDate().toLocal();
 
   @override
-  Timestamp toJson(DateTime value) => Timestamp.fromDate(value);
+  Timestamp? toJson(DateTime? value) =>
+      value == null ? null : Timestamp.fromDate(value);
 }
