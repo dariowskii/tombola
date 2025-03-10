25cf49b88fdcb61e457680aa0127ed4dc25408b8
Dario Varriale <dario.varriale@atobit.it>
2025-03-10 16:18:54 +0100

    Add raffleId parameter to GameSessionScreen constructor

--- lib/features/game_session_screen/presentation/game_session_screen.dart
+++ lib/features/game_session_screen/presentation/game_session_screen.dart
@@ -4,9 +4,11 @@ class GameSessionScreen extends StatelessWidget {
   const GameSessionScreen({
     super.key,
     required this.sessionId,
+    required this.raffleId,
   });
 
   final String sessionId;
+  final String raffleId;
 
   @override
   Widget build(BuildContext context) {
