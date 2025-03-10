c11f6d503a9855a042dd695be06623932dbf32ff
Dario Varriale <dario.varriale@atobit.it>
2025-03-10 18:09:16 +0100

    Add modal for username input to create or recover raffle cards

--- lib/features/setup_game_screen/presentation/setup_game_form.dart
+++ lib/features/setup_game_screen/presentation/setup_game_form.dart
@@ -5,6 +5,23 @@ import 'package:tombola/router/routes.dart';
 import 'package:tombola/utils/constants.dart';
 import 'package:tombola/utils/extensions.dart';
 
+enum ActionType {
+  create,
+  recover;
+
+  String get modalTitle => switch (this) {
+        ActionType.create => 'Nuova cartella',
+        ActionType.recover => 'Recupera cartella',
+      };
+
+  String get modalContent => switch (this) {
+        ActionType.create =>
+          'Inserisci lo username a cui associare la cartella',
+        ActionType.recover =>
+          'Inserisci lo username per recuperare la cartella',
+      };
+}
+
 class SetupGameForm extends ConsumerStatefulWidget {
   const SetupGameForm({
     super.key,
@@ -66,6 +83,83 @@ class _SetupGameFormState extends ConsumerState<SetupGameForm> {
     }
   }
 
+  void _showInsertUsernameModal(ActionType actionType) async {
+    await showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(
+                Spacing.medium.value,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    width: 40,
+                    height: 4,
+                    decoration: BoxDecoration(
+                      color: context.colorScheme.onSurface,
+                      borderRadius: BorderRadius.circular(2),
+                    ),
+                  ),
+                  Spacing.medium.h,
+                  Text(
+                    actionType.modalTitle,
+                    style: context.textTheme.titleLarge?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Spacing.medium.h,
+                  Text(
+                    actionType.modalContent,
+                    style: context.textTheme.labelLarge,
+                  ),
+                  Spacing.medium.h,
+                  Form(
+                    key: _formKey,
+                    child: TextFormField(
+                      controller: _usernameController,
+                      autofillHints: [AutofillHints.username],
+                      decoration: InputDecoration(
+                        label: const Text('Username'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Inserisci uno username';
+                        }
+                        return null;
+                      },
+                      onTapOutside: (_) {
+                        FocusManager.instance.primaryFocus?.unfocus();
+                      },
+                    ),
+                  ),
+                  Spacing.large.h,
+                  SizedBox(
+                    width: double.infinity,
+                    child: FilledButton(
+                      onPressed: () {},
+                      child: const Text('Conferma'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     if (!widget.isActive) {
@@ -108,6 +202,22 @@ class _SetupGameFormState extends ConsumerState<SetupGameForm> {
       );
     }
 
-    return const Placeholder();
+    return Column(
+      children: [
+        FilledButton(
+          onPressed: () => _showInsertUsernameModal(ActionType.create),
+          child: const Text(
+            'Genera una nuova cartella',
+          ),
+        ),
+        Spacing.small.h,
+        const Text('- oppure -'),
+        Spacing.small.h,
+        FilledButton(
+          onPressed: () => _showInsertUsernameModal(ActionType.recover),
+          child: const Text('Recupera la tua cartella esistente'),
+        ),
+      ],
+    );
   }
 }
