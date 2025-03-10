f6153d6104ef99698e122416f1482bf3c1d3684f
Dario Varriale <dario.varriale@atobit.it>
2025-03-10 16:44:31 +0100

    Add SetupGameBody widget as a ConsumerWidget for game session setup

---
+++ lib/features/setup_game_screen/data/setup_game_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SetupGameBody extends ConsumerWidget {
+  const SetupGameBody({
+    super.key,
+    required this.sessionId,
+  });
+
+  final String sessionId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
