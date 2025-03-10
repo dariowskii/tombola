e5567ba9abaa2b9bfbdc3163632f80bbc04cbc68
Dario Varriale <dario.varriale@atobit.it>
2025-03-10 18:28:48 +0100

    Implement session validation and duplicate check in generateRaffleCard function

--- lib/features/setup_game_screen/data/generate_raffle_card.dart
+++ lib/features/setup_game_screen/data/generate_raffle_card.dart
@@ -1,9 +1,15 @@
 import 'dart:math';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:tombola/models/game_session.dart';
+import 'package:tombola/providers/check_session_code_provider.dart';
+import 'package:tombola/providers/recover_raffle_card.dart';
 import 'package:tombola/utils/constants.dart';
+import 'package:tombola/utils/extensions.dart';
+
+part 'generate_raffle_card.g.dart';
 
 @riverpod
 Future<RaffleCard> generateRaffleCard(
@@ -11,6 +17,24 @@ Future<RaffleCard> generateRaffleCard(
   required String sessionId,
   required String username,
 }) async {
+  // Check if the session is active
+  final session = await ref.refresh(getSessionProvider(sessionId).future);
+  if (!session.isActive) {
+    throw 'Sessione non attiva';
+  }
+
+  // Check if raffle card with the same username already exists
+  final raffleCard = await ref.read(
+    recoverRaffleCardProvider(
+      sessionId: sessionId,
+      username: username,
+    ).future,
+  );
+
+  if (raffleCard != null) {
+    throw 'Cartella già esistente';
+  }
+
   final firstRow = List<int>.filled(9, 0);
   final secondRow = List<int>.filled(9, 0);
   final thirdRow = List<int>.filled(9, 0);
@@ -53,9 +77,21 @@ Future<RaffleCard> generateRaffleCard(
     thirdRow[column] = number;
   }
 
-  return RaffleCard(
-    id: sessionId,
+  var newRaffleCard = RaffleCard(
+    id: '',
     username: username,
     numbers: [...firstRow, ...secondRow, ...thirdRow],
   );
+
+  final sessionsCollection = FirebaseFirestore.instance.sessions;
+  final docReference =
+      await sessionsCollection.doc(sessionId).collection('raffles').add(
+            newRaffleCard.toJson(),
+          );
+
+  newRaffleCard = newRaffleCard.copyWith(
+    id: docReference.id,
+  );
+
+  return newRaffleCard;
 }
