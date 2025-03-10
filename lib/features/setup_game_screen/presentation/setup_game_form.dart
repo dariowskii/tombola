3b5756b28f44759fad4b90c612522865f7f3ddf5
Dario Varriale <dario.varriale@atobit.it>
2025-03-10 17:06:53 +0100

    Add SetupGameForm widget for session management

---
+++ lib/features/setup_game_screen/presentation/setup_game_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SetupGameForm extends StatelessWidget {
+  const SetupGameForm({
+    super.key,
+    required this.sessionId,
+  });
+
+  final String sessionId;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
