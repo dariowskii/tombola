948fd72fbbeab84ec7a6e54ed02ee82fd20529b9
Dario Varriale <dario.varriale@atobit.it>
2025-03-10 22:14:33 +0100

    Refactor SessionsList to improve UI structure and enhance loading state handling

--- lib/features/admin_screen/presentation/sessions_list.dart
+++ lib/features/admin_screen/presentation/sessions_list.dart
@@ -10,71 +10,75 @@ class SessionsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Object?>>(
-      stream: FirebaseFirestore.instance.sessions
-          .orderBy('createdAt', descending: true)
-          .snapshots(),
-      builder: (context, asyncSnapshot) {
-        if (asyncSnapshot.hasError) {
-          return Center(
-            child: Text(
-              'Errore: ${asyncSnapshot.error}',
-              style: context.textTheme.bodyLarge,
-            ),
-          );
-        }
-
-        if (asyncSnapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-
-        final sessions = (asyncSnapshot.data?.docs ?? []).map((e) {
-          final data = e.data() as Map<String, dynamic>;
-          data['id'] = e.id;
-          return GameSession.fromJson(data);
-        }).toList();
-
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(
-                Spacing.medium.value,
-              ),
+    return SafeArea(
+      child: StreamBuilder<QuerySnapshot<Object?>>(
+        stream: FirebaseFirestore.instance.sessions
+            .orderBy('createdAt', descending: true)
+            .snapshots(),
+        builder: (context, asyncSnapshot) {
+          if (asyncSnapshot.hasError) {
+            return Center(
               child: Text(
-                'Sessioni',
-                style: context.textTheme.titleLarge?.copyWith(
-                  fontWeight: FontWeight.bold,
+                'Errore: ${asyncSnapshot.error}',
+                style: context.textTheme.bodyLarge,
+              ),
+            );
+          }
+
+          if (asyncSnapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final sessions = (asyncSnapshot.data?.docs ?? []).map((e) {
+            final data = e.data() as Map<String, dynamic>;
+            data['id'] = e.id;
+            return GameSession.fromJson(data);
+          }).toList();
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(
+                  Spacing.medium.value,
+                ),
+                child: Text(
+                  'Sessioni',
+                  style: context.textTheme.titleLarge?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              child: sessions.isEmpty
-                  ? Center(
-                      child: Text(
-                        'Nessuna sessione trovata',
-                        style: context.textTheme.bodyLarge,
+              Expanded(
+                child: sessions.isEmpty
+                    ? Center(
+                        child: Text(
+                          'Nessuna sessione trovata',
+                          style: context.textTheme.titleLarge?.copyWith(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    : ListView.separated(
+                        itemCount: sessions.length,
+                        padding: EdgeInsets.all(
+                          Spacing.medium.value,
+                        ),
+                        separatorBuilder: (_, __) => Spacing.small.h,
+                        itemBuilder: (context, index) {
+                          final session = sessions[index];
+                          return SessionCardPreview(
+                            session: session,
+                          );
+                        },
                       ),
-                    )
-                  : ListView.separated(
-                      itemCount: sessions.length,
-                      padding: EdgeInsets.all(
-                        Spacing.medium.value,
-                      ),
-                      separatorBuilder: (_, __) => Spacing.small.h,
-                      itemBuilder: (context, index) {
-                        final session = sessions[index];
-                        return SessionCardPreview(
-                          session: session,
-                        );
-                      },
-                    ),
-            ),
-          ],
-        );
-      },
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
