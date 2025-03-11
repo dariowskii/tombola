2527086f2cbd621b362f9739fc8e86a724e0e98e
Dario Varriale <dario.varriale@atobit.it>
2025-03-11 18:15:18 +0100

    Adjust login screen layout to be responsive based on screen width

--- lib/features/login_screen/presentation/login_screen.dart
+++ lib/features/login_screen/presentation/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tombola/router/routes.dart';
@@ -70,6 +72,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final double minSize = min(500, context.width * 0.8);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Admin Login'),
@@ -79,110 +82,113 @@ class _LoginScreenState extends State<LoginScreen> {
           Spacing.medium.value,
         ),
         child: Center(
-          child: AutofillGroup(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    enabled: !_isLogging,
-                    controller: _emailController,
-                    textInputAction: TextInputAction.next,
-                    keyboardType: TextInputType.emailAddress,
-                    autofillHints: [AutofillHints.email],
-                    onTap: _isLogging
-                        ? null
-                        : () {
-                            Scrollable.ensureVisible(
-                              context,
-                              duration: 500.ms,
-                              curve: Curves.decelerate,
-                            );
-                          },
-                    onTapUpOutside: (_) {
-                      FocusManager.instance.primaryFocus?.unfocus();
-                    },
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Inserisci la l\'email';
-                      }
+          child: SizedBox(
+            width: minSize,
+            child: AutofillGroup(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      enabled: !_isLogging,
+                      controller: _emailController,
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.emailAddress,
+                      autofillHints: [AutofillHints.email],
+                      onTap: _isLogging
+                          ? null
+                          : () {
+                              Scrollable.ensureVisible(
+                                context,
+                                duration: 500.ms,
+                                curve: Curves.decelerate,
+                              );
+                            },
+                      onTapUpOutside: (_) {
+                        FocusManager.instance.primaryFocus?.unfocus();
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Inserisci la l\'email';
+                        }
 
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                      label: Text('Email'),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(16),
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        label: Text('Email'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(16),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Spacing.small.h,
-                  TextFormField(
-                    enabled: !_isLogging,
-                    controller: _passwordController,
-                    textInputAction: TextInputAction.done,
-                    keyboardType: TextInputType.visiblePassword,
-                    autofillHints: [AutofillHints.password],
-                    obscureText: true,
-                    onTap: _isLogging
-                        ? null
-                        : () {
-                            Scrollable.ensureVisible(
-                              context,
-                              duration: 500.ms,
-                              curve: Curves.decelerate,
-                            );
-                          },
-                    onTapUpOutside: (_) {
-                      FocusManager.instance.primaryFocus?.unfocus();
-                    },
-                    onFieldSubmitted: (_) {
-                      _tryLogin();
-                    },
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Inserisci la password';
-                      }
+                    Spacing.small.h,
+                    TextFormField(
+                      enabled: !_isLogging,
+                      controller: _passwordController,
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.visiblePassword,
+                      autofillHints: [AutofillHints.password],
+                      obscureText: true,
+                      onTap: _isLogging
+                          ? null
+                          : () {
+                              Scrollable.ensureVisible(
+                                context,
+                                duration: 500.ms,
+                                curve: Curves.decelerate,
+                              );
+                            },
+                      onTapUpOutside: (_) {
+                        FocusManager.instance.primaryFocus?.unfocus();
+                      },
+                      onFieldSubmitted: (_) {
+                        _tryLogin();
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Inserisci la password';
+                        }
 
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                      label: Text('Password'),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(16),
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        label: Text('Password'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(16),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Spacing.large.h,
-                  SizedBox(
-                    width: double.infinity,
-                    child: FilledButton(
-                      onPressed: _isLogging ? null : _tryLogin,
-                      child: _isLogging
-                          ? Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Text('Entra'),
-                                Spacing.medium.w,
-                                SizedBox(
-                                  width: 16,
-                                  height: 16,
-                                  child: CircularProgressIndicator(
-                                    strokeWidth: 2,
-                                    color: context.colorScheme.onPrimary,
+                    Spacing.large.h,
+                    SizedBox(
+                      width: double.infinity,
+                      child: FilledButton(
+                        onPressed: _isLogging ? null : _tryLogin,
+                        child: _isLogging
+                            ? Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text('Entra'),
+                                  Spacing.medium.w,
+                                  SizedBox(
+                                    width: 16,
+                                    height: 16,
+                                    child: CircularProgressIndicator(
+                                      strokeWidth: 2,
+                                      color: context.colorScheme.onPrimary,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            )
-                          : const Text('Entra'),
+                                ],
+                              )
+                            : const Text('Entra'),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
