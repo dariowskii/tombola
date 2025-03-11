3f5af12c9ec67d3a3610b013cbc7625c682e7d0e
Dario Varriale <dario.varriale@atobit.it>
2025-03-11 18:03:50 +0100

    Enable path URL strategy in main application

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:tombola/firebase_options.dart';
 import 'package:tombola/router/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  usePathUrlStrategy();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
